9d4049a00d44aeb09e668b41c8d06e06733bb486
BrianAmin <tuf49787@temple.edu>
2018-10-09 14:21:27 -0400

    Laying out code, added in output function. Adjusted some variable names

--- project_1/project_1.ino
+++ project_1/project_1.ino
@@ -12,6 +12,9 @@ https://stackoverflow.com/questions/30263913/how-to-implement-8-bit-dac-digital-
 
 */
 
+// Arduino Low Pass filter example:
+// https://helpful.knobs-dials.com/index.php/Low-pass_filter#Arduino_example
+
 // Analog Pin 0 for Signal Input
 // https://www.arduino.cc/en/Tutorial/AnalogInputPins
 #define INPUT_PIN 0;
@@ -26,46 +29,57 @@ https://stackoverflow.com/questions/30263913/how-to-implement-8-bit-dac-digital-
 // Pin will output HIGH when we start and end processing
 int PIN_STAT = 12;
 int PIN_ANALOG_IN = 0;
+int PIN_OUT = 2; //PWM PIN
 
 
-uint10_t buffer[SAMPLE_NUMBER];
+unsigned int input_buffer[SAMPLE_NUMBER];
+float output_buffer[SAMPLE_NUMBER];
 int pos = 0;
 
 int CONSTANT_OFFSET = 512;
 
 void input_handler()
 {
-    uint10_t value = analogRead(PIN_ANALOG_IN);
+    unsigned int value = analogRead(PIN_ANALOG_IN);
     buffer[pos++] = value;
 
 
 }
 
-void processing(uint10_t buffer[], int size)
+// fs is proportional to the no. of quantization levels in case of smooth signals like your sine wave.
+
+double processing(unsigned int buffer[], int size)
 {
     // Begin Processing
     digitalWrite(PIN_STAT, HIGH);
 
     // Convert from unsigned int to signed int
-    //float signed_buffer[size];
     for (int i = 0; i <= size; i++)
     {
         int signed_int = buffer[i] - CONSTANT_OFFSET;
-        Serial.println("Unsigned Int: ");
-        Serial.println(buffer[i]);
-        Serial.println("Signed Int: ")
-        Serial.println(signed_int);
-        //signed_buffer[i] = (float)signed_int;
+        signed_buffer[i] = (float)signed_int;
 
     }
+    // Run through Low pass filter:
 
-    // Print results
-    Serial.println("Printing Results");
+
+
+    // Cast back to 8bit
+
+
+    // Pull to show processing is completed
+    digitalWrite(PIN_STAT, LOW);
+}
+
+
+// Output filtered signal
+void output_data(float output_buffer[], int size)
+{
     for (int i = 0; i <= size; i++)
     {
-        Serial.println(signed_buffer[i]);
+        Serial.println(output_buffer[i]);
+        analogWrite(PIN_OUT, output_buffer[i]);
     }
-    digitalWrite(PIN_STAT, LOW);
 }
 
 
@@ -84,11 +98,11 @@ void setup()
 
 void loop()
 {
-    if (pos == SAMPLE_NUMBER)
-    {
-        Serial.println("Done Collecting");
-        processing(buffer, SAMPLE_NUMBER);
-    }
+
+    // O P R as discussed in class
+    output_data(output_buffer, SAMPLE_NUMBER);
+
+    processing(input_buffer, SAMPLE_NUMBER);
 
 
   
