32ea2743e412130c30b7cbd104dbde787aa18f30
BrianAmin <tuf49787@temple.edu>
2018-10-10 11:39:42 -0400

    Adjusted floating point math, lowpass filter output works. Now need to work on conversions for PWM

--- project_1/project_1.ino
+++ project_1/project_1.ino
@@ -1,4 +1,5 @@
-
+#include <FilterOnePole.h>
+#include <Filters.h>
 #include <TimerThree.h>
 
 
@@ -10,27 +11,32 @@
 
 // Recrunch these values after figuring out sampling freq
 #define SAMPLE_NUMBER 50 //number of samples
-#define SAMPLE_FREQ 20 // samples per second (Hz)
+#define SAMPLE_FREQ 1000 // samples per second (Hz)
 #define SAMPLE_PERIOD 0.05 // Period 1/SAMPLE_FREQ (seconds)
 
 
 int CONSTANT_OFFSET = 512;
-
+float lowpass_cutoff = 25.00; // Hz
+FilterOnePole lowpassFilt(LOWPASS, lowpass_cutoff);
 
 void input_handler()
 {
     unsigned int value = analogRead(PIN_ANALOG_IN);
     int signed_int = value - CONSTANT_OFFSET;
     float signed_value = (float)signed_int;
-    //value = lowpassFilter.output() + CONSTANT_OFFSET;
-    //int analog_bits = value * (5/1023);
+    lowpassFilt.input(signed_value);
+
+
+    float val = lowpassFilt.output() + CONSTANT_OFFSET;
+    float analog_bits = val * (0.004887);
     //analog_bits = map(analog_bits, 0, 1023, 0, 255);
-    float output = fir.process(signed_value);
+    Serial.println(analog_bits);
+    //analogWrite(PIN_OUT, analog_bits);
 
 
 
 
-     Serial.println(output);
+     //Serial.println(output);
 
 
 
@@ -43,7 +49,7 @@ void setup()
 {
     Serial.begin(115200);
     Timer3.attachInterrupt(input_handler);
-    Timer3.setPeriod(50000); //in microseconds
+    Timer3.setPeriod(1000); //in microseconds
     Serial.println("Starting to sample");
     Timer3.start();
   
