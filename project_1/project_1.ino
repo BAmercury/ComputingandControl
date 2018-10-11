26bbdd9a7d37c2ec677d68cb004f9c90c1185eb1
BrianAmin <tuf49787@temple.edu>
2018-10-11 01:36:32 -0400

    Adjusted sampling frequency to prof's recommendations. Just need to figure out how to properly output PWM

--- project_1/project_1.ino
+++ project_1/project_1.ino
@@ -1,26 +1,29 @@
 #include <FilterOnePole.h>
 #include <Filters.h>
 #include <TimerThree.h>
+#include <TimerOne.h>
 
 
 #define PIN_STAT 12 // Pin will output HIGH when we start and end processing
 #define PIN_ANALOG_IN 0
-// #define PIN_OUT 9 // For Arduino UNO
-#define PIN_OUT 2 // PWM PIN
+#define PIN_OUT 3 // PWM PIN
 
 
 // Recrunch these values after figuring out sampling freq
-#define SAMPLE_NUMBER 50 //number of samples
-#define SAMPLE_FREQ 1000 // samples per second (Hz)
-#define SAMPLE_PERIOD 0.05 // Period 1/SAMPLE_FREQ (seconds)
+#define SAMPLE_FREQ 500 // samples per second (Hz)
+#define SAMPLE_PERIOD 0.002 // Period 1/SAMPLE_FREQ (seconds)
+#define SAMPLE_PERIOD_US 2000 // In microseconds
 
 
 int CONSTANT_OFFSET = 512;
 float lowpass_cutoff = 25.00; // Hz
 FilterOnePole lowpassFilt(LOWPASS, lowpass_cutoff);
 
+uint8_t int_pwm = 0;
+
 void input_handler()
 {
+
     unsigned int value = analogRead(PIN_ANALOG_IN);
     int signed_int = value - CONSTANT_OFFSET;
     float signed_value = (float)signed_int;
@@ -28,35 +31,31 @@ void input_handler()
 
 
     float val = lowpassFilt.output() + CONSTANT_OFFSET;
-    float analog_bits = val * (0.004887);
-    //analog_bits = map(analog_bits, 0, 1023, 0, 255);
-    Serial.println(analog_bits);
-    //analogWrite(PIN_OUT, analog_bits);
+    // Convert from bits to bipolar analog bits (5/1023)
+    //int_pwm = map(val, 0, 1023, 0, 255);
 
 
-
-
-     //Serial.println(output);
-
-
-
-
+    float analog_bits = val * (0.00488758553);
+    //  Quantize to 8 bits
+    float p_w_m = analog_bits * (51);
+    int_pwm = (uint8_t)p_w_m;
 }
-
 
 
 void setup()
 {
     Serial.begin(115200);
+    pinMode(PIN_OUT, OUTPUT);
     Timer3.attachInterrupt(input_handler);
-    Timer3.setPeriod(1000); //in microseconds
-    Serial.println("Starting to sample");
+    Timer3.setPeriod(SAMPLE_PERIOD_US); //in microseconds
     Timer3.start();
   
 }
 
 
 void loop()
-{ 
+{
+
+
 }
 
