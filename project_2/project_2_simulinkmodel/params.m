1cbd9250c85d41bc53de235fd64a2c5f6872ab34
BrianAmin <tuf49787@temple.edu>
2018-11-02 22:41:10 -0400

    Model is completed, needs to be veryfied

--- project_2/project_2_simulinkmodel/params.m
+++ project_2/project_2_simulinkmodel/params.m
@@ -1,20 +1,26 @@
-%% file:  Setup_HIL
-% Dr. Tom Chmielewski
-% 20 October 2018
 %% System Parameters
 % a = 1/(RC)
 a = 25; % radians
 Fs = 500;% Hz
 T = 1/Fs;
-
-K = 1;
+% Output Signal Period
+U = 1/Fs;
+computation_time = 0.01;
+IRQ_DELAY = T + computation_time; % Interrupt service routine delay + sampling period
 % try K = 10 you get an overshot with time dealy approx
+K = 1;
+
+
+
+
+%% z transform model G(z) 
+
 % K sets pole of analog system to
 pa = -a-K*a
 
 %%
 Tsim = 0.0001 % for Simulink fixed point solver
 
-%% z transform model G(z) 
+
 zp=exp(-a*T)
 zg=K*(1-exp(-a*T))
